289ad909bd3658ca7d006f97ec110c76e3cdb335
Yrii Pereskokov <skulines@mail.ru>
2015-11-04 15:27:21 +0500

    C2
    C2

--- vendor/ko-ko-ko/assert/.provision/exec/01.packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-apt-get update > /dev/null 2>&1
-
-apt-get install -y vim mc htop \
-    git mercurial \
-    curl \
-    php-pear php5-dev php5-cli php5-curl php5-xdebug > /dev/null 2>&1
-
--- vendor/ko-ko-ko/assert/.provision/files/dot/.vimrc
+++
@@ -1,6 +0,0 @@
-set nocompatible
-filetype off
-set paste
-set tabstop=4
-set number
-
